e363845bd4d1740ccd6719d0758dbf7397d9953d
Anton_Stepanov <antonsm@mail.ru>
2024-03-11 22:26:40 +0400

    sem1

--- seminar/sem1/Program.cs
+++ seminar/sem1/Program.cs
@@ -50,15 +50,22 @@
 // 782 => 9
 // 918 => 17
 
-System.Console.Write("Input three-digit number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if (num >= 100 && num <= 999)
-{
-    int ed = num % 10;
-    int sot = num / 100;
-    System.Console.WriteLine($"{num} -> {ed + sot}");
-}
-else
-{
-    System.Console.Write("You Input not three-digit number");
-}
+// System.Console.Write("Input three-digit number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num >= 100 && num <= 999)
+// {
+//     int ed = num % 10;
+//     int sot = num / 100;
+//     System.Console.WriteLine($"{num} -> {ed + sot}");
+// }
+// else
+// {
+//     System.Console.Write("You Input not three-digit number");
+// }
+
+// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
