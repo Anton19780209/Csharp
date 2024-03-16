d90eb34903bc48d0f42bb13bd8642452a2ff449f
Anton_Stepanov <antonsm@mail.ru>
2024-03-16 14:05:51 +0400

    tas32l2

---
+++ lesson/lesson2/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿//int n = 10;
+int[] array = [4, 5, 9, 3, 6, 8, 2, 6, 4, 3];
+int i = 0;
+
+while (i < array.Length)
+{  
+    if (array[i] % 2 == 0)
+    {
+        System.Console.Write($"{array[i]} ");
+    
+    }
+    i = i + 1;
+}
