23558f8c7dba082409bd2481f96f8a9eb9080ab8
Anton_Stepanov <antonsm@mail.ru>
2024-03-16 10:53:54 +0400

    task4

---
+++ lesson/lesson1/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 6;
+int b = 8;
+int c = 3;
+int d = 1;
+int e = 9;
+
+int max = a;
+
+if (b > a)
+{
+    max = b;
+}
+if (c > b)
+{
+    max = c;
+}
+if (d > c)
+{
+    max = d;
+}
+if (e > d)
+{
+    max = e;
+}
+
+System.Console.WriteLine(max);
