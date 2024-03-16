e49a38a5fc83bbc07b8c0713c8ead98b570c4780
Anton_Stepanov <antonsm@mail.ru>
2024-03-16 18:36:23 +0400

    sem1

---
+++ seminar/sem1/task2s1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход
+// принимает два целых числа и проверяет,
+// является ли первое число квадратом второго.
+// a = 25, b = 5 => да
+// a = 2, b = 10 => нет
+// a = 9, b = -3 => да
+// a = -3, b = 9 => нет
+
+System.Console.Write("Input ferst number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b * b == a)
+{
+    System.Console.WriteLine($"{a} is quard of {b}");
+}
+else
+{
+    System.Console.WriteLine($"{a} is not quard of {b}");
+}
---
+++ seminar/sem1/task4s1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает сумму
+// первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
+
+System.Console.Write("Input three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 100 && num <= 999) 
+{
+    int ed = num % 10;
+    int sot = num / 100;
+    System.Console.WriteLine($"{num} -> {ed + sot}");
+}
+else
+{
+    System.Console.Write("You Input not three-digit number");
+}
+
+// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
