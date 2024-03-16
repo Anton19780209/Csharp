84f9a558b7e7840b048f7c8f7b2e0f7441096a24
Anton_Stepanov <antonsm@mail.ru>
2024-03-16 15:01:07 +0400

    task4l2

---
+++ lesson/lesson2/Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿int n = 5;
+int[] array = { 2, 8, 5, 16, 7 };
+int i = 0;
+int max = array[i];
+
+while (i < n)
+{
+    if (array[i] > max)
+    {
+       max = array[i];
+    }
+    i = i + 1;
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+for (int j = 1; j < n; j++)
+{
+    if (array[j] > max)
+    {
+       max = array[j];
+    }
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+       max = e;
+    }
+}
+System.Console.WriteLine(max);
