d9baf621fda110f2210e43cf315829094354aa3d
Anton_Stepanov <antonsm@mail.ru>
2024-03-17 13:11:44 +0400

    sem

---
+++ seminar/sem2/task2s2/Program.cs
@@ -0,0 +1,77 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и возводит вторую цифру этого
+// числа в степень, равную третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+//Задание 3. Работа в сессионных залах
+//Семинар 2. Простые алгоритмы на C#
+//Напишите программу, которая будет принимать на
+//вход два числа и выводить, является ли первое число
+//кратным второму. Если первое число некратно
+//второму, то программа выводит остаток от деления.
+//Примеры
+//14, 5 => нет, 4
+//16, 8 => да
+//4, 3 => нет, 1
+//
+void MinusSecond(int num)//456
+{
+    int ed = num % 10;//6
+    int sot = num / 100;//4
+    System.Console.WriteLine($"{num} -> {sot * 10 + ed}");
+}
+
+int MinusSecondInt(int num)//456
+{
+    int ed = num % 10;//6
+    int sot = num / 100;//4
+    //System.Console.WriteLine($"{num} -> {sot * 10 + ed}");
+    int result = sot * 10 + ed;
+    return result;
+}
+
+int SecondToThird(int num)
+{
+    int ed = num % 10;
+    int dec = num / 10 % 10;
+    int res = 1;
+    for (int i = 0; i < ed; i++)//2^4 = 2 * 2 * 2 * 2
+    {
+        res = res * dec;
+    }
+    return res;
+}
+
+bool IsTheeDigit(int num)
+{
+    if (num >= 100 && num <= 999)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void FirstDivSecond(int a, int b)
+{
+    if(a % b == 0)
+    {
+        System.Console.WriteLine($"{a} кратно {b}");
+    }
+    else
+    {
+        System.Console.WriteLine($"{a} не кратно {b}, остаток {a % b}");
+    }
+}
+
+System.Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+FirstDivSecond(num1, num2);
---
+++ seminar/sem3/task1s3/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задание 1. Совместная работа
+// Семинар 3. Массивы
+// 15 мин
+// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да
+
+// Задайте массив из 10 элементов, заполненный числами из
+// промежутка [-10, 10]. Замените отрицательные элементы на
+// положительные, а положительные на отрицательные.
+// Пример
+// [1 -5 6]
+// => [-1 5 -6]
+
+
+
+int[] CreateArray(int n, int min, int max)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+// bool FindNumber(int num, int[] array)
+// {
+//     foreach (int item in array)
+//     {
+//         if (item == num)
+//         {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+int[] SingInvert(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+    return array;
+}
+
+System.Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input minimal value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input maximal value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArray(size, min, max);
+PrintArray(arr);
+PrintArray(SingInvert(arr));
+
+int[] arrey = CreateArray(size, min, max);
+PrintArray(arr);
+System.Console.WriteLine();
+System.Console.Write("Input num wants to find: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(FindNumber(num, arr));
+
+// Найдите произведения пар чисел в одномерном массиве. Парой
+// считаем первый и последний элемент, второй и предпоследний и
+// т.д. Результат запишите в новый массив.
+// Пример
+// [1 3 2 4 2 3] => [3 6 8]
+// [2 3 1 7 5 6 3] => [6 18 5]
