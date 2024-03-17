291c4adf02768fadd1cee4c1827f9b1bbb38e3a9
Anton_Stepanov <antonsm@mail.ru>
2024-03-17 15:44:51 +0400

    task1s3

---
+++ seminar/sem3/task1.1s3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив из 10 элементов, заполненный числами из
+// промежутка [-10, 10]. Замените отрицательные элементы на
+// положительные, а положительные на отрицательные.
+// Пример
+// [1 -5 6]
+// => [-1 5 -6]
+
+bool FindNumber(int num, int[] array)
+{
+    foreach (int item in array)
+    {
+        if (item == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int[] SingInvert(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+    return array;
+}
+
+
+
+int[] arr = CreateArray(size, min, max);
+PrintArray(arr);
+PrintArray(SingInvert(arr));
+
+
+
+
