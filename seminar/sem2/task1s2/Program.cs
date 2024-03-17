1f4646d174bcbe0c8a68f8e34bd3b2f20968e31d
Anton_Stepanov <antonsm@mail.ru>
2024-03-17 13:10:43 +0400

    task1s2

---
+++ seminar/sem2/task1s2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+// void MinusSecond (int num)
+// {
+//     int ed = num % 10;
+//     int sot = num / 100;
+//     System.Console.WriteLine($"{num} -> {sot * 10 + ed}");
+// }
+
+// System.Console.Write("Input three-digit number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// MinusSecond(a);
+
+// second metodate
+
+int MinusSecondInt (int num)
+{
+    int ed = num % 10;
+    int sot = num / 100;
+    // System.Console.WriteLine($"{num} -> {sot * 10 + ed}");
+    int result = sot * 10 + ed;
+    return result;
+}
+
+System.Console.Write("Input three-digit number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int res = MinusSecondInt (a);
+System.Console.WriteLine (res);
+
